19e7a30d865956e27cb754233af6e23a2c5f0a5a
preethzcodez <preethzcodez.dll@gmail.com>
2017-07-12 18:36:55 +0530

    initial commit

---
+++ app/src/main/java/com/preethzcodez/wifiadvanced/MainActivity.java
@@ -0,0 +1,13 @@
+package com.preethzcodez.wifiadvanced;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wifi_demo_layout);
+    }
+}
